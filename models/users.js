5fcb38d73283b45efc5ae058391281353b929a59
ThsPrdct <thomas.prodct@gmail.com>
2023-02-21 01:45:47 +0100

    backend

--- models/users.js
+++ models/users.js
@@ -3,8 +3,7 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   username: String,
   password: String,
-  
-  
+  token: String,  
 });
 
 // Modèle
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+const fetch = require('node-fetch');
+
+
+router.get('/trends', (req, res) => {
+  fetch('http://localhost:3001/feed/:token')
+    .then(response => response.json())
+    .then(data => {
+      if (data.status === 'ok') {
+        res.json({ articles: data.articles });
+      } else {
+        res.json({ articles: [] });
+      }
+    });
+});
 
 module.exports = router;
